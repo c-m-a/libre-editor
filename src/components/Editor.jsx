35aa4afa9b91df505f563ac0e249782008aa282b
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2024-01-28 17:10:40 -0500

    Refactor: moving to parent the object that contains the code

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,18 +1,21 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-export default function Editor() {
-  const codeString = "(num) => num + 1";
-  const codeExample = {
-    lang: "javascript",
-    code: "(num) => num + 1;",
-  };
-
+export default function Editor({ code }) {
   return (
-    <>
-      <SyntaxHighlighter language={codeExample.lang} style={atomOneDark}>
-        {codeString}
+    <section className="flex h-1/2 w-full md:h-full md:w-1/2">
+      <ul className="w-10 pr-2 pt-2 bg-[#282c34]">
+        <li className="text-right">1</li>
+        <li className="text-right">2</li>
+      </ul>
+      <SyntaxHighlighter
+        className="w-full"
+        language={code.lang}
+        style={atomOneDark}
+        wrapLongLines={true}
+      >
+        {code.text}
       </SyntaxHighlighter>
-    </>
+    </section>
   );
 }
