a94bafa69111b297f98aedd151bf05050a57b783
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2024-01-28 09:08:57 -0500

    Add: styles to create the 3 top left buttons in the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,12 @@
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="flex py-1 px-3">
+      <ul className="flex items-center mr-6 gap-2">
+        <li className="w-3 h-3 rounded-full bg-red-500"></li>
+        <li className="w-3 h-3 rounded-full bg-yellow-500"></li>
+        <li className="w-3 h-3 rounded-full bg-green-500"></li>
+      </ul>
+      <div>Example</div>
+    </header>
+  );
 }
