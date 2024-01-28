9497485e514f61edef79bae154a62fea601a641d
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2024-01-28 17:06:04 -0500

    Add: styles to create the window in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+  const code = {
+    lang: "javascript",
+    text: "(num) => num + 1;",
+  };
+
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
+    <div className="bg-gray-500 flex md:grid md:place-items-center flex-1 h-svh shadow p-4 text-white">
+      <div className="flex flex-col bg-[#3a404d] overflow-hidden rounded-md h-full w-full">
+        <Header />
+        <Main code={code} />
+        <Footer code={code} />
+      </div>
     </div>
   );
 }
